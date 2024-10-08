c03005ca7b724c3b6ab90c2b3ffd47e178805584
Arittro7 <arittro007@gmail.com>
2024-10-08 11:58:04 +0600

    Cookie

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+// const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+// app.use(cookieParser())
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wv413.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,9 +34,25 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('bookings');
     const productCollection = client.db('carDoctor').collection('products');
     const teamCollection = client.db('carDoctor').collection('teams');
 
+    // Auth related API
+    app.post('/jwt', async (req, res) =>{
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user , process.env.ACCESS_SECRET_TOKEN, {expiresIn:'1h'} )
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'none'
+      })
+      .send({success: true})
+    })
+
+    // Services related API
     app.get('/services', async(req, res) =>{
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -52,10 +75,54 @@ async function run() {
       const query = {_id: new ObjectId(id)}
 
       const options = {
-        projection: {title: 1}
+        projection: {title: 1, price: 1, img: 1}
       };
 
       const result = await serviceCollection.findOne(query, options);
+      res.send(result)
+    })
+    // Booking related                        🚩
+    // Post booking data
+    app.post('/bookings', async(req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result)
+    })
+
+    // get booking data
+    app.get('/bookings', async(req, res) =>{
+      console.log(req.query.email);
+      console.log('Booking', req.cookies.token);
+      let query ={}
+      if (req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    // Update booking data
+    app.patch('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+          $set: {
+              status: updatedBooking.status
+          },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+  })
+
+
+    // Delete Booking data
+    app.delete('/bookings/:id', async (req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await bookingCollection.deleteOne(query);
       res.send(result)
     })
 
@@ -68,7 +135,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 app.get('/',  (req, res) => {
   res.send('Doctor server is running')
